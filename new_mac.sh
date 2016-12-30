24826df1b0083e8104964b6a4cb49e1b3548fc2d
邓丹 <dengdan890730@163.com>
2016-12-30 21:48:56 +0800

    a lot of modifications

--- new_mac.sh
+++ new_mac.sh
@@ -2,3 +2,6 @@ scp dengdan@10.76.0.115:/home/dengda/.vimrc .
 scp dengdan@10.76.0.115:/home/dengda/.tmux.conf .
 scp dengdan@10.76.0.115:/home/dengda/.theanorc .
 
+kinit
+ln -s /mnt/nfs/dengdan/github/ .
+ln -s /mnt/nfs/dengdan/temp/ .
