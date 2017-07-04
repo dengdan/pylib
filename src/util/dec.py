834c0b164db396c53316b788bb0ee22fc0f46773
dengdan <dengdan890730@163.com>
2017-07-04 15:32:14 +0800

    ...

--- src/util/dec.py
+++ src/util/dec.py
@@ -35,4 +35,23 @@ def print_calling_in_short(fn):
         return ret
     return wrapper
 
+import collections
+counter = collections.defaultdict(int)
+def print_calling_in_short_for_tf(fn):
+    import tensorflow as tf
+    import util
+    def wrapper(*args1, ** args2):
+        start = time.time()
+        thread_name = util.thread.get_current_thread_name()
+        ret = fn(*args1, **args2)
+        end = time.time()
+        counter[fn.__name__] = counter[fn.__name__] + (end - start)
+        all_time = sum([counter[name] for name in counter]) * 1.0
+        for name in counter:
+            tf.logging.info('\t %s: %f, %f seconds'%(name, counter[name] / all_time, counter[name]))
+        s = "Thread [%s]:function [%s] has been called, taking %f seconds"%(thread_name, fn.__name__, (end - start))
+        tf.logging.info(s)
+        return ret
+    return wrapper
+
 
--- src/util/thread_.py
+++ src/util/thread_.py
@@ -3,6 +3,9 @@ from threading import Thread
 
 def get_current_thread():
     return threading.current_thread()
+
+def get_current_thread_name():
+    return get_current_thread().getName()
     
 def is_alive(t):
     return t.is_alive()
