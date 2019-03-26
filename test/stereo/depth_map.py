ed696c8463b8ca382e0e3a28db39698657fe7994
dengdan <dengdan.zju@gmail.com>
2019-03-26 16:25:09 +0800

    ..

--- test/stereo/depth_map.py
+++ test/stereo/depth_map.py
@@ -3,7 +3,9 @@ import numpy as np
 import cv2
 import pdb
 from matplotlib import pyplot as plt
-calibration_path = "/data/data_arc/truck1_bino_20190301+20190227_calibration/calibration_45_20190302153703.json"
+from mpl_toolkits.mplot3d import Axes3D
+import pptk
+calibration_path = "~/remote/desktop/data/data_arc/truck1_bino_20190301+20190227_calibration/calibration_45_20190302153703.json"
 data = util.io.load_json(calibration_path)
 camera_model = util.stereo.BinocularModel(**data)
 
@@ -18,13 +20,19 @@ img3d = cv2.reprojectImageTo3D(disp, camera_model.Q)
 Xs = img3d[..., 0].ravel()
 Ys = img3d[..., 1].ravel()
 Zs = img3d[..., 2].ravel()
+
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(Xs, Ys, Zs, c='r', marker='o')
-ax.set_xlabel('Y')
-ax.set_ylabel('Z')
-ax.set_zlabel('X')
+ax = Axes3D(fig)
+ax.scatter(Xs, Ys, Zs, s = 0.5)
 plt.show()
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(Xs, Ys, Zs, c='r', marker='o')
+# ax.set_xlabel('Y')
+# ax.set_ylabel('Z')
+# ax.set_zlabel('X')
+# plt.show()
 
 depth = img3d[..., 2]
 # pdb.set_trace()
@@ -33,7 +41,7 @@ depth = img3d[..., 2]
 rect = [(920, 700), (1600, 1200)]
 car_area = depth[rect[0][1]:rect[1][1], rect[0][0]:rect[1][0]]
 # util.plt.imshow("", car_area)
-# v = pptk.viewer(img3d)
-# v.set(point_size=0.005)
-
+v = pptk.viewer(img3d)
+v.set(point_size=0.005)
+v.wait()
 print(car_area.shape)
