528916e369c14dbf39e02467daa9cd4b735ad3d9
dengdan <dengdan.zju@gmail.com>
2019-09-30 12:07:45 +0800

    add gpu_ids in tf.get_available_gpus

--- src/util/tf.py
+++ src/util/tf.py
@@ -13,19 +13,21 @@ except:
 def is_gpu_available():
     return tf.test.is_gpu_available()
 
-def get_available_gpus(num_gpus = None):
+def get_available_gpus(num_gpus = None, gpu_ids = None):
     """
     Modified on http://stackoverflow.com/questions/38559755/how-to-get-current-available-gpus-in-tensorflow
     However, the original code will occupy all available gpu memory.
     The modified code need a parameter: num_gpus. It does nothing but return the device handler name
     It will work well on single-maching-training, but I don't know whether it will work well on a cluster.
     """
+    if gpu_ids is not None:
+        num_gpus = len(gpu_ids.split(","))
     if num_gpus == None:
         from tensorflow.python.client import device_lib as _device_lib
         local_device_protos = _device_lib.list_local_devices()
         return [x.name for x in local_device_protos if x.device_type == 'GPU']
     else:
-        return ['/gpu:%d'%(idx) for idx in xrange(num_gpus)]
+        return ['/gpu:%d'%(idx) for idx in range(num_gpus)]
 
 def get_latest_ckpt(path):
 # tf.train.latest_checkpoint
